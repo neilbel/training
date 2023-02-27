b9ea34a61dd6d4f959c2fd794ebad3265e7f9959
neilbel <55654909+neilbel@users.noreply.github.com>
2023-02-27 19:30:05 +0100

    Add files via upload

---
+++ Programmation/Python/exos_anciens/01/fibo.py
@@ -0,0 +1,31 @@
+"""
+Neil Bel Hadj
+Suite de Fibonacci récursive et itérative
+"""
+
+# fibonacci récursive
+# bien (élégante) mais très gourmande
+# def fibo(n):
+#     if n < 2:
+#         return 1
+#     else:
+#         return fibo(n - 1) + fibo(n - 2)
+
+# fibonacci itérative
+# utilise c : courant, p : précédent et pp : précédent de précédent
+def fibo(n):
+    if n < 2:
+        return 1
+    if n == 2:
+        return 2
+    p = 1
+    c = 2
+    for i in range(3, n + 1):
+        pp = p
+        p = c
+        c = pp + p
+    ratio = p / c
+    print("meilleure proportion de la règle d'or :", ratio * 100, " -", (1 - ratio) * 100)
+    return c
+
+print(fibo(20000))
---
+++ Programmation/Python/exos_anciens/01/reverse.py
@@ -0,0 +1,34 @@
+def reverse(t):
+    i=0
+    j=len(t)-1
+    while (i<j):
+        tmp=t[i]
+        t[i]=t[j]
+        t[j]=tmp
+        i+=1
+        j-=1
+    return t
+
+def reverse2(t):
+    rt=[]
+    for x in t:
+        rt.insert(0,x)
+    return rt
+        
+def test():
+    tab=[1,9,14,6,48,-12]
+    print ("          tab :", tab)
+    print (" reverse(tab) :", reverse(tab))
+    print ("          tab :", tab)
+    print ("reverse2(tab) :", reverse2(tab))
+    print ("          tab :", tab)
+    print ("j'appelle tab.reverse()")
+    tab.reverse()
+    print ("          tab :", tab)
+    print ("j'appelle tab.sort()")
+    tab.sort()
+    print ("          tab :", tab)
+    
+test()
+
+
